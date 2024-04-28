37be8a6eb28d0190e8db31f9455b1b9363efa070
Ramesh Nair <ram@hiddentao.com>
2024-04-28 12:23:23 +0100

    feat: cors working for server

--- packages/server/next.config.js
+++ packages/server/next.config.js
@@ -3,32 +3,6 @@ const { NormalModuleReplacementPlugin } = require('webpack');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  headers: async () => {
-    return [
-      {
-        // Routes this applies to
-        source: '/api/(.*)',
-        // Headers
-        headers: [
-          // Allow for specific domains to have access or * for all
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          // Allows for specific methods accepted
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, PUT, DELETE, OPTIONS',
-          },
-          // Allows for specific headers accepted (These are a few standard ones)
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Content-Type, Authorization',
-          },
-        ],
-      },
-    ];
-  },
   output: 'standalone',
   webpack: (config) => {
     // see https://github.com/sindresorhus/got/issues/2267#issuecomment-1659768856
--- packages/server/pages/api/graphql.ts
+++ packages/server/pages/api/graphql.ts
@@ -1,4 +1,4 @@
-import get from 'lodash.get';
+import NextCors from 'nextjs-cors';
 import { parse } from 'url';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { ApolloServer, HeaderMap } from '@apollo/server';
@@ -28,6 +28,15 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
+  // Run the cors middleware
+  // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'OPTIONS', 'POST'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
   const headers = new HeaderMap();
 
   for (const [key, value] of Object.entries(req.headers)) {
